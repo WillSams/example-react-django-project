9dd47261981e026b4830ad5ca1ab9213f2cc98ce
Will Sams <will@samswebs.com>
2024-03-05 08:43:30 -0500

    Add working frontend

---
+++ frontend/src/screens/index.ts
@@ -0,0 +1,13 @@
+import HomeComponent from './Home';
+import ModifyReservationComponent from './Reservations/Modify';
+import BookReservationComponent from './Reservations/Book';
+import ViewReservationComponent from './Reservations/View';
+import StaticComponent from './Static';
+
+export default {
+  HomeComponent,
+  ModifyReservationComponent,
+  BookReservationComponent,
+  ViewReservationComponent,
+  StaticComponent,
+};
